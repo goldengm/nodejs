e9097189c20d3d9b6664b6b686d2bb69a0530a6d
leo <leokokko1022@gmail.com>
2019-12-05 23:07:28 +0800

    Updated proxyURL

--- puppeteer/task1.js
+++ puppeteer/task1.js
@@ -1,9 +1,9 @@
 const rp = require('request-promise');
 const cheerio = require("cheerio");
 const url = "http://ec2-18-221-228-218.us-east-2.compute.amazonaws.com/shop?load_products=1&min_price=0&max_price=596&filters_applied=1&options=1&options_value=3&Colors%5B%5D=Red&type=desc&limit=15&page_number=1";
-
+const proxyURL = 'http://108.59.3.139:18001@165.227.118.92:8002';
 (async() => {
-    const proxiedRequest = rp.defaults({ proxy: 'http://108.59.3.139:18001@165.227.118.92:8002' });
+    const proxiedRequest = rp.defaults({ proxy: proxyURL });
     this.response = await proxiedRequest.get(url);
     
     try {
